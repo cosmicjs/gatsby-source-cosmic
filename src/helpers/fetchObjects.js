90e2c8c05b6270f86b01816bd70070614e3d402d
pendragon-engineering <king.donovan+alias@gmail.com>
2022-11-09 12:49:32 -0800

    feat(fetch): wIP Fetch function for object types

--- src/helpers/fetchObjects.js
+++ src/helpers/fetchObjects.js
@@ -1,16 +1,44 @@
 import Cosmic from 'cosmicjs';
+import async from 'async';
 
-const api = Cosmic();
-
-const fetchObjects = async ({ reporter }, { bucketSlug, readKey, objectTypes, limit }) => {
+const fetchObjects = async (
+  { reporter },
+  {
+    bucketSlug, readKey, objectTypes,
+  },
+) => {
+  const api = Cosmic();
   const bucket = api.bucket({
     slug: bucketSlug,
     read_key: readKey,
   });
 
-  const objects = [];
-  const promises = [];
-  // TODO: WIP
+  const fetchedObjects = await async.mapSeries(
+    objectTypes,
+    async (objectType) => {
+      try {
+        // TODO: Add support for pagination fetching.
+        const { objects } = await bucket.objects
+          .find({
+            // TODO: Add support for querying from object type config.
+            type: objectType.slug,
+          })
+          .limit(objectType.limit);
+        reporter.info(`Fetched ${objects.length} objects for type ${objectType.slug}.`);
+        console.log('objects', objects);
+        return {
+          ...objectType,
+          objects,
+        };
+      } catch (error) {
+        // TODO: Improve error handling & logging.
+        reporter.error(`Error fetching ${objectType.slug}: `, error);
+        return [];
+      }
+    },
+  );
+
+  return fetchedObjects;
 };
 
 export default fetchObjects;
