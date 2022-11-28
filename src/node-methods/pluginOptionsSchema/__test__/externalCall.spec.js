45e160e5e6c3990cb08d823885ea961641249dca
pendragon-engineering <king.donovan+alias@gmail.com>
2022-11-27 16:37:48 -0800

    test(coverage): 100% test coverage; refactored plugin options; improve build system

---
+++ src/node-methods/pluginOptionsSchema/__test__/externalCall.spec.js
@@ -0,0 +1,50 @@
+/* eslint-disable camelcase */
+// eslint-disable-next-line no-unused-vars
+import Cosmic from 'cosmicjs';
+import externalValidator from '../externalValidator';
+
+jest.mock('cosmicjs', () => () => ({
+  bucket: jest.fn(({ read_key }) => ({
+    getObjectTypes: read_key === 'fail' ? jest.fn().mockRejectedValue({ status: 401, message: 'Unauthorized' })
+      : jest.fn().mockResolvedValue({}),
+  })),
+}));
+
+describe('pluginOptionsSchema', () => {
+  describe('externalValidator', () => {
+    it('should not throw when connection test is successful', async () => {
+      const options = {
+        bucketSlug: 'fakeBucketSlug',
+        readKey: 'fakeRead',
+      };
+
+      let error;
+
+      try {
+        await externalValidator(options);
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error).toBeUndefined();
+    });
+
+    it('should throw error if connection fails', async () => {
+      const options = {
+        bucketSlug: 'fakeBucketSlug',
+        readKey: 'fail',
+      };
+
+      let error;
+
+      try {
+        await externalValidator(options);
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error).toBeDefined();
+      expect(error.message).toEqual('There was an issue connecting to Cosmic.\nStatus Code: 401\nMessage: Unauthorized');
+    });
+  });
+});
