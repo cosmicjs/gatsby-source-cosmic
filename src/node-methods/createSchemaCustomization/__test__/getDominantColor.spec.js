8cd81a11618bd77b088b867a56e75844cf0c344a
pendragon-engineering <king.donovan+alias@gmail.com>
2023-01-24 18:48:17 -0800

    refactor(color): remove fallback to sharp, it was causing more trouble than it's worth

--- src/node-methods/createSchemaCustomization/__test__/getDominantColor.spec.js
+++ src/node-methods/createSchemaCustomization/__test__/getDominantColor.spec.js
@@ -1,24 +1,26 @@
 import * as axios from 'axios';
-import * as gatsbyCoreUtils from 'gatsby-core-utils';
-import * as pluginSharp from 'gatsby-plugin-sharp';
 import getDominantColor, {
   fetchedDomColorImages,
-  inProgressDomColorImages,
 } from '../getDominantColor';
 
 jest.mock('axios');
-jest.mock('gatsby-core-utils');
-jest.mock('gatsby-plugin-sharp');
 
 describe('getDominantColor', () => {
+  const reporter = {
+    info: jest.fn(),
+    warn: jest.fn(),
+    panic: jest.fn(),
+  };
+
   beforeEach(() => {
-    jest.clearAllMocks();
+    axios.mockClear();
+    delete fetchedDomColorImages['https://imgix.cosmicjs.com/image.jpg'];
   });
 
   it('should return a default color if no image is provided', async () => {
     const imageURL = null;
 
-    const result = await getDominantColor(imageURL);
+    const result = await getDominantColor(imageURL, { reporter });
 
     expect(result).toEqual('rgba(0,0,0,0.5)');
   });
@@ -26,45 +28,86 @@ describe('getDominantColor', () => {
   it('should return a default color if the image is not from imgix', async () => {
     const imageURL = 'https://test.com/image.jpg';
 
-    const result = await getDominantColor(imageURL);
+    const result = await getDominantColor(imageURL, { reporter });
 
     expect(result).toEqual('rgba(0,0,0,0.5)');
   });
 
   it('should attempt to get color from imgix', async () => {
     const imageURL = 'https://imgix.cosmicjs.com/image.jpg';
-    const cacheMock = { directory: 'test' };
     const axiosMock = axios.get.mockImplementation(() => Promise.resolve({
       data: {
         dominant_colors: {
           vibrant_dark: {
-            hex: '#000000',
+            hex: '#94622d',
           },
         },
       },
     }));
 
-    const result = await getDominantColor(imageURL, cacheMock);
+    const result = await getDominantColor(imageURL, { reporter });
 
-    expect(result).toEqual('#000000');
+    expect(result).toEqual('#94622d');
     expect(axiosMock).toHaveBeenCalledWith(`${imageURL}&palette=json`);
   });
 
-  //  TODO: This test is failing
-  it('should use sharp if imgix fails', async () => {
+  it('should store a fetched color in the fetchedDomColorImages object', async () => {
     const imageURL = 'https://imgix.cosmicjs.com/image.jpg';
-    const cacheMock = { directory: 'test' };
-    const axiosMock = axios.get.mockImplementation(() => Promise.reject(new Error('test')));
-    // const md5Mock = gatsbyCoreUtils.md5.mockImplementation(() => Promise.resolve('test'));
-    // const fetchRemoteFileMock = gatsbyCoreUtils.fetchRemoteFile.mockImplementation(() => Promise.resolve('test'));
-    const sharpMock = pluginSharp.getDominantColor.mockImplementation(() => '#000000');
+    axios.get.mockImplementation(() => Promise.resolve({
+      data: {
+        dominant_colors: {
+          vibrant_dark: {
+            hex: '#94622d',
+          },
+        },
+      },
+    }));
 
-    const result = await getDominantColor(imageURL, cacheMock);
+    await getDominantColor(imageURL, { reporter });
 
-    expect(result).toEqual('#000000');
+    expect(fetchedDomColorImages[imageURL]).toEqual('#94622d');
+  });
+
+  it('should retrieve a color from the fetchedDomColorImages object if it has already been fetched', async () => {
+    const imageURL = 'https://imgix.cosmicjs.com/image.jpg';
+    fetchedDomColorImages[imageURL] = '#94622d';
+
+    const result = await getDominantColor(imageURL, { reporter });
+
+    expect(result).toEqual('#94622d');
+    expect(axios.get).not.toHaveBeenCalled();
+  });
+
+  it('should return a default color if imgix returns no dominant color', async () => {
+    const imageURL = 'https://imgix.cosmicjs.com/image.jpg';
+    const axiosMock = axios.get.mockImplementation(() => Promise.resolve({
+      data: {
+        dominant_colors: {},
+      },
+    }));
+
+    const result = await getDominantColor(imageURL, { reporter });
+
+    expect(result).toEqual('rgba(0,0,0,0.5)');
     expect(axiosMock).toHaveBeenCalledWith(`${imageURL}&palette=json`);
-    // expect(md5Mock).toHaveBeenCalledWith(`${imageURL}__DOMINANT_COLOR`);
-    // expect(fetchRemoteFileMock).toHaveBeenCalled();
-    // expect(sharpMock).toHaveBeenCalled();
+  });
+
+  it('should return a default color if the axios promise rejects', async () => {
+    const imageURL = 'https://imgix.cosmicjs.com/image.jpg';
+    const axiosMock = axios.get.mockImplementation(() => Promise.reject());
+
+    const result = await getDominantColor(imageURL, { reporter });
+
+    expect(result).toEqual('rgba(0,0,0,0.5)');
+    expect(axiosMock).toHaveBeenCalledWith(`${imageURL}&palette=json`);
+  });
+
+  it('should report a warning if imgix fails', async () => {
+    const imageURL = 'https://imgix.cosmicjs.com/image.jpg';
+    axios.get.mockImplementation(() => Promise.reject());
+
+    await getDominantColor(imageURL, { reporter });
+
+    expect(reporter.warn).toHaveBeenCalledWith(`Could not get dominant color from imgix for image: ${imageURL}`);
   });
 });
--- src/node-methods/createSchemaCustomization/getDominantColor.js
+++ src/node-methods/createSchemaCustomization/getDominantColor.js
@@ -1,19 +1,16 @@
 /* eslint-disable camelcase */
 import axios from 'axios';
-import mime from 'mime-types';
-import { fetchRemoteFile } from 'gatsby-core-utils';
-import { md5 } from 'hash-wasm';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import pluginSharp from 'gatsby-plugin-sharp';
 
 const fetchedDomColorImages = {};
-const inProgressDomColorImages = {};
-const getDominantColor = async (imageURL, cache) => {
+const getDominantColor = async (imageURL, { reporter }) => {
   const defaultColor = 'rgba(0,0,0,0.5)';
   if (!imageURL) return defaultColor;
 
   // Require the image to be coming from cosmic's imgix cdn
   if (!imageURL.startsWith('https://imgix.cosmicjs.com')) return defaultColor;
+
+  // Check if we already have the color return it
+  if (fetchedDomColorImages[imageURL]) return fetchedDomColorImages[imageURL];
 
   // Attempt to get the dominant color from imgix
   // TODO: We could also expose which color to use as a query setting
@@ -26,48 +23,14 @@ const getDominantColor = async (imageURL, cache) => {
       return dominant_colors.vibrant_dark.hex;
     }
   } catch (e) {
-    console.warn('Could not get dominant color from imgix\nfalling back to sharp', e);
+    reporter.warn(`Could not get dominant color from imgix for image: ${imageURL}`);
   }
 
-  // Check if we already have the color return it
-  if (fetchedDomColorImages[imageURL]) return fetchedDomColorImages[imageURL];
-  // Check if we are already fetching the image and return the promise
-  if (inProgressDomColorImages[imageURL]) return inProgressDomColorImages[imageURL];
-
-  const fetchFile = async (url, directory) => {
-    const ext = mime.extension(mime.lookup(url.split('?')[0]));
-    const cacheKey = await md5(`${url}__DOMINANT_COLOR`);
-    const path = await fetchRemoteFile({
-      url,
-      directory,
-      ext,
-      cacheKey,
-    });
-    return path;
-  };
-
-  const imagePromise = fetchFile(imageURL, cache.directory);
-  inProgressDomColorImages[imageURL] = imagePromise;
-
-  return imagePromise.then((path) => {
-    // TODO: Has Own check
-    const color = pluginSharp.getDominantColor(path);
-    fetchedDomColorImages[imageURL] = color;
-    delete inProgressDomColorImages[imageURL];
-    return color;
-  }).catch((e) => {
-    // TODO: Use reporter instead of console.error
-    console.error(
-      '[gatsby-source-cosmic] Could not getDominantColor from image',
-      e,
-    );
-    return 'rgba(0,0,0,0.5)';
-  });
+  return defaultColor;
 };
 
 export {
   fetchedDomColorImages,
-  inProgressDomColorImages,
 };
 
 export default getDominantColor;
